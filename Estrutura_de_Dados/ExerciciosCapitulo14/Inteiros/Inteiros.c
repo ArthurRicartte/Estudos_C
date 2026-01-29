066a992448e6ab23e84d6cb27b55efbb445d1c0b
Arthur Ricartte <a.ricartte@gmail.com>
2026-01-29 15:33:57 -0300

    Recriando o arquivo.c do capítulo 14
    Acabei apagando o arquivo ponto c do TAD de inteiros do capítulo 14.

---
+++ Estrutura_de_Dados/ExerciciosCapitulo14/Inteiros/Inteiros.c
@@ -0,0 +1,630 @@
+#include "Inteiros.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+//Desenvolvendo estruturas:
+struct inteiro{
+    int info;
+    Inteiro* prox;
+};
+
+struct listaInt{
+    Inteiro* cabeca;
+};
+
+//Criando lista vazia:
+ListaInt* cria_list(){
+    //Criando lista e alocando memoria:
+    ListaInt* lista = (ListaInt*) malloc(sizeof(ListaInt));
+
+    //Verificando se deu certo:
+    if (!lista){
+        printf("ERRO AO ALOCAR MEMORIA (1)\n");
+        exit(1);
+    }
+
+    //Modificando ponteiro de lista:
+    lista -> cabeca = NULL;
+
+    return lista;
+}
+
+//Inserir no inicio da lista:
+void insere_inicio(int num, ListaInt* lista){
+    //Verificando procedencia da lista:
+    if (!lista){
+        printf("ERROR LISTA INVALIDA (2)\n");
+        return;
+    }
+
+    //Criando um novo no:
+    Inteiro* novo = (Inteiro*) malloc(sizeof(Inteiro));
+
+    //Verificando se no foi alocado dinamicamente:
+    if (!novo){
+        printf("ERRO AO ALOCAR MEMORIA PARA O NOVO NO (3)\n");
+        return;
+    }
+
+    //Novo No recebe num:
+    novo -> info = num;
+
+    //Novo No aponta para a antiga cabeca:
+    novo -> prox = lista -> cabeca;
+    
+    //Cabeca passa a ser o novo No:
+    lista -> cabeca = novo;
+}
+
+//Libera lista da memoria:
+void libera(ListaInt* lista){
+    //verifica se a lista existe:
+    if (!lista){
+        printf("ERRO LISTA INVALIDA(4)\n");
+        return;
+    }
+
+    //Percorrendo lista para liberar cada No:
+    Inteiro* atual = lista -> cabeca;
+    Inteiro* aux = NULL;
+    
+    while (atual != NULL){
+        aux = atual;
+        atual = atual -> prox;
+        free(aux);
+    }
+    
+    //Liberando a estrutura da memoria:
+    free(lista);
+}
+
+//Exibe Lista:
+void exibirLista(ListaInt* lista){
+    //verifica se a lista eh vazia:
+    if (!lista){
+        printf("ERRO: PONTEIRO DE LISTA INVALIDO (5)\n");
+        return;
+    }
+
+    // Lista vazia:
+    if (lista -> cabeca == NULL){
+        printf("Lista vazia\n");
+        return;
+    }
+
+    //Percorrendo lista para exibir:
+    Inteiro* atual = lista -> cabeca;
+    int cont = 0;
+   
+    while (atual != NULL){
+        
+        printf("No %d || Valor: %d ||", cont, atual -> info);
+
+        //Determinando a info do prox:
+        if (atual -> prox != NULL){
+            printf("Prox: %d\n", atual -> prox -> info);
+        }else{
+            printf("NULL\n");
+        }
+        
+        atual = atual -> prox;
+        cont++;
+    }
+}
+
+//Questao 1: Uma funcao que retorne o comprimento da lista (Quantos nos ela tem:)
+int comprimento (ListaInt* l){
+    //Verificando se a lista eh valida:
+    if (!l){
+        return -1; //Deu errado
+    }
+
+    //Percorrendo a lista para determinar a quantidade de nos:
+    int contador = 0;
+    Inteiro* atual = l -> cabeca;
+
+    while (atual != NULL){
+        contador++;
+        atual = atual -> prox;
+    }
+
+    //retornando a quantidade de nos:
+    return contador;
+}
+
+//Questao 2: uma funcao que retorna a quantida de nos que tenham info maior que um x:
+int maiores (ListaInt* l, int x){
+    //Verificando se a lista eh valida:
+    if (!l){
+        return -1; //Deu errado
+    }
+
+    //Percorrendo a lista para determinar a quantidade de nos:
+    int contador = 0;
+    Inteiro* atual = l -> cabeca;
+
+    while (atual != NULL){
+        
+        //Verificando se existe algum no com info maior que x:
+        if (atual -> info > x){
+            contador++;
+        }
+        atual = atual -> prox;
+    }
+
+    //retornando a quantidade de nos especificos:
+    return contador;
+}
+
+//Questao 3: retorna o ultimo valor da lista:
+int ultimo (ListaInt* l){
+    //Verificando se a lista eh valida:
+    if (!l){
+        return -1; //Deu errado
+    }
+
+    //retornando -2 caso a lsita seja vazia:
+    if (l -> cabeca == NULL){
+        return -2;
+    }
+    
+    //Percorrendo a lista para determinar o ultimo no:
+    Inteiro* atual = l -> cabeca;
+
+    while (atual != NULL){
+        
+        //Verificando ultimo elemento:
+        if (atual -> prox == NULL){
+            return atual -> info;
+        }
+        atual = atual -> prox;
+    }
+
+    //Caso alguma coisa inesperada aconteca:
+    return -3;
+}
+
+//Questao 4: concatena 2 listas:
+void concatena (ListaInt* l1 , ListaInt* l2 ){
+    //Verificar se as listas sao validas:
+    if (!l1 || !l2){
+        printf("ERRO: PONTEIRO DE LISTA INVALIDO (6)\n");
+        return;
+    }
+
+    //Caso 1: l2 vazia = faz nada
+    if (l2 -> cabeca == NULL){
+        return;
+    }
+
+    //Caso 2: l1 vazia = recebe l2
+    if (l1 -> cabeca == NULL){
+        l1 -> cabeca = l2 -> cabeca;
+        l2 -> cabeca = NULL;
+        return;
+    }
+
+    //Caso 3: Ambas nao vazias = encontrar fim de l1
+    Inteiro* atual = l1 -> cabeca;
+
+    //Percorrendo ate o ultimo no de l1:
+    while (atual -> prox != NULL){
+        atual = atual -> prox;
+    }
+
+    //Concatenando l1 e l2:
+    atual -> prox = l2 -> cabeca;
+    l2 -> cabeca = NULL;
+}
+
+//Questao 5: retira as ocorrencias de um valor x na lista:
+void retira_n (ListaInt* l, int x){
+    //verifica se a lista eh vazia:
+    if (!l){
+        printf("ERRO: PONTEIRO DE LISTA INVALIDO (7)\n");
+        return;
+    }
+
+    //Criando variaveis auxiliares:
+    Inteiro* atual = l -> cabeca;
+    Inteiro* ant = NULL;
+    Inteiro* aux = NULL;
+
+    //Percorrendo a lista:
+    while (atual != NULL){
+
+        //Verificando se o atual deve ser removido:
+        if (atual -> info == x){
+           
+            //Guardando o no a ser removido:
+            aux = atual;
+
+            if (ant == NULL){    //Caso seja a cabeca:
+                l -> cabeca = atual -> prox;
+            }else{   //Caso nao seja a cabeca:
+                ant -> prox = atual -> prox;
+            }
+
+            //Removendo o no:
+            atual = atual -> prox;
+            free(aux);
+
+        }else{
+            //Caso nao seja um valor a ser removido, avanca os ponteiros:
+            ant = atual;
+            atual = atual -> prox;
+        }
+    }
+}
+
+//Questaoo 6: separa a lista em duas a partir de um valor x:
+ListaInt* separa (ListaInt* l, int x){
+    //Verificando se a lista eh valida://verifica se a lista eh vazia:
+    if (!l){
+        printf("ERRO: PONTEIRO DE LISTA INVALIDO (8)\n");
+        return NULL;
+    }
+
+    //Criando variaveis auxiliares:
+    Inteiro* atual = l -> cabeca;
+    Inteiro* ant = NULL;
+
+    //Percorrendo a lista:
+    while (atual != NULL && atual -> info != x){
+        ant = atual;
+        atual = atual -> prox;
+    }
+
+    //Verificando se encontrou o valor x:
+    if (atual == NULL){
+        //Caso nao encontre, retorna lista vazia:
+        return cria_list();
+    }
+
+    //Criando nova lista:
+    ListaInt* novaLista = cria_list();
+    if (!novaLista){
+        printf("ERROR: FALHA AO ALOCAR MEMORIA PARA NOVA LISTA (9)\n");
+        return NULL;
+    }
+
+    //Nova lista comeca no sucessor do atual:
+    novaLista -> cabeca = atual -> prox;
+
+    //A lista original termina no atual:
+    atual -> prox = NULL;
+
+    //Retornando a nova lista:
+    return novaLista;
+}
+
+//Questao 7: Intercalar os valores de duas listas em uma terceira lista:
+ListaInt* merge (ListaInt* l1 , ListaInt* l2 ){
+    //Verificando se as listas sao validas:
+    if (!l1 || !l2){
+        printf("ERRO: PONTEIRO DE LISTA INVALIDO (8)\n");
+        return NULL;
+    }
+
+    //Criando a nova lista:
+    ListaInt* novaLista = cria_list();
+
+    //Verificando se a nova lista foi criada:
+    if (!novaLista){
+        printf("ERRO: FALHA AO ALOCAR MEMORIA PARA NOVA LISTA (9)\n");
+        return NULL;
+    }
+
+    //Criando ponteiros auxiliares:
+    Inteiro* atual1 = l1 -> cabeca;
+    Inteiro* atual2 = l2 -> cabeca;
+    Inteiro* ultimo = NULL;
+
+    //Percorrendo ambas listas:
+    while (atual1 != NULL || atual2 != NULL){
+        //Se ainda houver elementos em l1, adiciona uma copia:
+        if (atual1 != NULL){
+            //Tentando alocar memoria:
+            Inteiro* novo = (Inteiro*) malloc (sizeof(Inteiro));
+
+            //Verificando se alocacao deu certo:
+            if (!novo){
+                printf("ERRO: FALHA AO ALOCAR MEMORIA PARA NOVO NO (10)\n");
+                libera(novaLista);
+                return NULL;
+            }
+
+            //Alocacao deu certo, preenchendo o novo no:
+            novo -> info = atual1 -> info;
+            novo -> prox = NULL;
+
+            //Verificando se eh o primeiro no da nova lista:
+            if (novaLista -> cabeca == NULL){
+                novaLista -> cabeca = novo;
+            }else{
+                ultimo -> prox = novo;
+            }
+
+            //Atualizando o ultimo no:
+            ultimo = novo;
+            atual1 = atual1 -> prox;
+        }
+
+        //Se ainda houver elementos em l2, adiciona uma copia:
+        if (atual2 != NULL){
+            Inteiro* novo = (Inteiro*) malloc (sizeof(Inteiro));
+
+            //Verificando se alocacao deu certo:
+            if (!novo){
+                printf("ERRO: FALHA AO ALOCAR MEMORIA PARA NOVO NO (11)\n");
+                libera(novaLista);
+                return NULL;
+            }
+
+            //Alocacao deu certo, preenchendo o novo no:
+            novo -> info = atual2 -> info;
+            novo -> prox = NULL;
+
+            //Aqui adiciona a nova lista:
+            if (novaLista -> cabeca == NULL){
+                novaLista -> cabeca = novo;
+            }else{
+                ultimo -> prox = novo;
+            }
+
+            //Atualizando o ultimo no:
+            ultimo = novo;
+            atual2 = atual2 -> prox;
+        }
+    }
+
+    /*OBSERVACAO: A logica dessa funcao eh basicamente a mesma quando vc tenta alterar 
+    a cabeca de uma lista, nesse caso a gente vai mudando o ultimo valor 
+    (Faz ele apontar para o novo No criado).*/
+    
+    //Retornando a nova lista:
+    return novaLista;
+}
+
+//Questao 8: Inverter a lista:
+void inverte (ListaInt* l){
+    //Verificando se a lista eh valida:
+    if (!l){
+        printf("ERRO: PONTEIRO DE LISTA INVALIDO (12)\n");
+        return;
+    }
+
+    //Verificando se a lista eh vazia ou tem um unico elemento:
+    if (l -> cabeca == NULL || l -> cabeca -> prox == NULL){
+        return; //Nada a fazer
+    }
+
+    //Inicializando ponteiros auxiliares:
+    Inteiro* ant = NULL;
+    Inteiro* atual = l -> cabeca;
+    Inteiro* proximo = NULL;
+  
+    //Percorrendo a lista:
+    while (atual != NULL){
+        //Guarda o proximo no:
+        proximo = atual -> prox;
+
+        //Inverte o ponteiro do no atual:
+        atual -> prox = ant;
+
+        //Muda a refrencia de anterior e proximo:
+        ant = atual;
+        atual = proximo;
+    }
+
+    //Atualiza a cabeca da lista:
+    l -> cabeca = ant;
+}
+
+//Questao 9: Fazer uma copia da lista (De forma iterativa):
+ListaInt* copiaIterativa (ListaInt* l){
+    //Verificando se a lista eh valida:
+    if (!l){
+        printf("ERRO: PONTEIRO DE LISTA INVALIDO (13)\n");
+        return NULL;
+    }
+
+    //Criando a nova lista:
+    ListaInt* novaLista = cria_list();
+
+    //Verificando se a nova lista foi criada:
+    if (!novaLista){
+        printf("ERRO: FALHA AO ALOCAR MEMORIA PARA NOVA LISTA (14)\n");
+        return NULL;
+    }
+
+
+    //Caso a lista original esteja vazia:
+    if (l -> cabeca == NULL){
+        return novaLista;
+    }
+
+    //Criando variaveis auxiliares:
+    Inteiro* atual_orig = l -> cabeca;
+    Inteiro** atual_copia = &(novaLista -> cabeca);
+
+    
+    //Percorrendo a lista original:
+    while (atual_orig != NULL){
+        //Criando um novo no:
+        Inteiro* novo = (Inteiro*) malloc (sizeof(Inteiro));
+
+        //Verificando se funcionou:
+        if (!novo){
+            printf("ERRO: FALHA AO ALOCAR MEMORIA PARA NOVO NO (15)\n");
+            libera(novaLista);
+            return NULL;
+        }
+
+        //Copiando o valor:
+        novo -> info = atual_orig -> info;
+        novo -> prox = NULL;
+
+        //Conectando o novo no na copia:
+        *atual_copia = novo;
+        atual_copia = &(novo -> prox);
+
+        //Passando adiante:
+        atual_orig = atual_orig -> prox;
+    }
+
+    //Retornando a nova lista:
+    return novaLista;
+}
+
+//Questão 9: Fazer uma copia da lista (De forma recursiva):
+ListaInt* copiaRecursiva (ListaInt* l){
+    //Verificando se a lista eh valida:
+    if (!l){
+        printf("ERRO: PONTEIRO DE LISTA INVALIDO (16)\n");
+        return NULL;
+    }
+
+    //Verificando se a lista eh vazia:
+    if (l -> cabeca == NULL){
+        return cria_list();
+    }
+
+    //Criando nova lista:
+    ListaInt* novaLista = cria_list();
+
+    //Verificando se a nova lista foi criada:
+    if (!novaLista){
+        printf("ERRO: FALHA AO ALOCAR MEMORIA PARA NOVA LISTA (17)\n");
+        return NULL;
+    }
+
+    //Copia o no de forma recursiva:
+    novaLista -> cabeca = copiaNoRecursivo(l -> cabeca);
+
+    //Retorna a nova lista copiada:
+    return novaLista;
+}
+
+
+//Funcao que vai auxilar na copia recursiva:
+Inteiro* copiaNoRecursivo (Inteiro* no_Orig){
+    //Verificando se o no eh nulo:
+    if (!no_Orig){
+        return NULL;
+    }
+
+    //Criando novo no:
+    Inteiro* novo = (Inteiro*) malloc (sizeof(Inteiro));
+
+    //Verificando se a alocacao deu certo:
+    if (!novo){
+        printf("ERRO: FALHA AO ALOCAR MEMORIA PARA NOVO NO (18)\n");
+        return NULL;
+    }
+
+    //Copiando a informacao:
+    novo -> info = no_Orig -> info;
+
+    //Chamando a funcao recursivamente para o proximo no:
+    novo -> prox = copiaNoRecursivo(no_Orig -> prox);
+
+    //Retornando o novo no:
+    return novo;
+}
+
+//Questao 10: Transforma lista simplimente encadeada em lista circular:
+void para_circular (ListaInt* l){
+    //Verificando se a lista eh valida:
+    if (!l){
+        printf("ERRO: PONTEIRO DE LISTA INVALIDO (19)\n");
+        return;
+    }
+
+    //Verificando se a lista eh vazia:
+    if (l -> cabeca == NULL){
+        return; //Nada a fazer
+    }
+
+    //Criando variavel auxiliar:
+    Inteiro* atual = l -> cabeca;
+
+    //Percorrendo a lista:
+    while (atual -> prox != NULL){
+        atual = atual -> prox;
+    }
+
+    //Fazendo o ultimo no apontar para a cabeca:
+    atual -> prox = l -> cabeca;
+}
+
+//Funcao auxilar da questao 10: Exibir lista circular:
+void exibirListaCircular(ListaInt* lista){
+    //Verificar se a lista eh valida:
+    if (!lista){
+        printf("ERRO: PONTEIRO DE LISTA INVALIDO (20)\n");
+        return;
+    }
+
+    //Verificando se a lista eh vazia:
+    if (lista -> cabeca == NULL){
+        printf("Lista vazia\n");
+        return;
+    }
+
+    //Criando variavel auxiliar:
+    Inteiro* atual = lista -> cabeca;
+    int cont = 0;
+
+    //Percorrendo a lista circular:
+    while (1){
+        printf("No %d || Valor: %d", cont, atual -> info);
+
+        //Determinando a info do prox:
+        if (atual -> prox != NULL){
+            printf(" || Prox: %d\n", atual -> prox -> info);
+        }else{
+            printf(" || Prox: NULL\n");
+            break;  //Deu erro: lista circular mal feita
+        }
+
+        //Proximo no:
+        atual = atual -> prox;
+        cont++;
+
+        //Verifcando se voltou a cabeca:
+        if (atual == lista -> cabeca){
+            break;
+        }
+    }
+}
+
+//Funcao auxilar da questao 10: Liberar lista circular:
+void liberaCircular(ListaInt* lista){
+    //Verificar se a lista eh valida:
+    if (!lista){
+        printf("ERRO: PONTEIRO DE LISTA INVALIDO (21)\n");
+        return;
+    }
+
+    //Verificando se a lista eh vazia:
+    if (lista -> cabeca == NULL){
+        free(lista);
+        return;
+    }
+
+    //Criando variavel auxiliar:
+    Inteiro* atual = lista -> cabeca;
+    Inteiro* primeiro = lista -> cabeca;
+    Inteiro* aux = NULL;
+
+    //Percorrendo a lista circular:
+    do {
+        aux = atual;
+        atual = atual -> prox;
+        free(aux);
+    }while (atual != NULL && atual != primeiro);
+
+    //Liberando a estrutura da lista:
+    free(lista);
+}
